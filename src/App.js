071836791303a635e2548b2b64e93e25f3fd76c9
ManishMallik <manish03mallik@gmail.com>
2023-01-10 21:03:43 -0600

    Routing issues trying to be fixed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ToyotaTripsPage from './pages/ToyotaTrips';
 import RPG from './pages/RPG';
 import ClaimSafePage from './pages/ClaimSafe';
 //import logo from './images/mkm_logo.jpg';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter} from 'react-router-dom';
 //manishkmallik/
 
 function App() {
@@ -19,15 +19,17 @@ function App() {
         <Navigation/>
       </div>
       <div>
-        <BrowserRouter basename='/manishkmallik'>
-          <Routes>
-            <Route exact path="/" element={<Home/>} /> 
-            <Route path="/about" element={<About/>} />
-            <Route path="/projects" element={<Projects/>} />
-            <Route path="/toyotatrips" element={<ToyotaTripsPage/>} />
-            <Route path="/readypetgo" element={<RPG/>} />
-            <Route path="/claimsafe" element={<ClaimSafePage/>} />
-          </Routes>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <HashRouter>
+            <Routes>
+              <Route exact path="/" element={<Home/>} /> 
+              <Route path="/about" element={<About/>} />
+              <Route path="/projects" element={<Projects/>} />
+              <Route path="/toyotatrips" element={<ToyotaTripsPage/>} />
+              <Route path="/readypetgo" element={<RPG/>} />
+              <Route path="/claimsafe" element={<ClaimSafePage/>} />
+            </Routes>
+          </HashRouter>
         </BrowserRouter>
       </div>
       <div className='contact-bar'>
