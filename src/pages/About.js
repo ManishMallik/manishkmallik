d5f69713f08a83fd53722533400288cc844326b3
ManishMallik <manish03mallik@gmail.com>
2023-01-12 21:05:41 -0600

    Removing carousel

--- src/pages/About.js
+++ src/pages/About.js
@@ -40,29 +40,41 @@ const About = () => {
 			<br/>
 			<br/>
 
-			<Carousel variant="dark" style={{width: width/2.5, height: width/2.5}}>
-				<Carousel.Item>
-					<img src={Weightlifting} alt="Weightlifting" width={width/2.5} height={width/2.5}
-					/>
-					<Carousel.Caption>
-						<h5>Weightlifting</h5>
-					</Carousel.Caption>
-				</Carousel.Item>
-				<Carousel.Item>
-					<img src={Sports} alt="Sports" style={{backgroundColor: 'white'}} width={width/2.5} height={width/2.5}/>
-					<Carousel.Caption>
-						<h5>Sports</h5>
-					</Carousel.Caption>
-				</Carousel.Item>
-				<Carousel.Item>
-					<img src={Cooking} alt="Cooking" width={width/2.5} height={width/2.5}
-					/>
-					<Carousel.Caption>
-						<h5>Cooking</h5>
-					</Carousel.Caption>
-				</Carousel.Item>
-			</Carousel>
-
+			<Row>
+				<Col className='m-10' md={5} lg={2}>
+					<Figure className='figure-align'>
+						<Figure.Image width={width} height={width} alt="Manish" src={Weightlifting}/>
+						<Figure.Caption style={{color: 'white'}}>
+							Weightlifting
+						</Figure.Caption>
+					</Figure>
+				</Col>
+				<Col className='m-10' md={5} lg={2}>
+					<Figure className='figure-align'>
+						<Figure.Image width={width} height={width} alt="Manish" src={Sports} style={{backgroundColor: 'white'}}/>
+						<Figure.Caption style={{color: 'white'}}>
+							Sports
+						</Figure.Caption>
+					</Figure>
+				</Col>
+				<Col className='m-10' md={5} lg={2}>
+					<Figure className='figure-align'>
+						<Figure.Image width={width} height={width} alt="Manish" src={Cooking}/>
+						<Figure.Caption style={{color: 'white'}}>
+							Cooking
+						</Figure.Caption>
+					</Figure>
+				</Col>
+				<Col className='m-10' md={5} lg={2}>
+					<Figure className='figure-align'>
+						<Figure.Image width={width} height={width} alt="Manish" src={Networking}/>
+						<Figure.Caption style={{color: 'white'}}>
+							Networking with professionals
+						</Figure.Caption>
+					</Figure>
+				</Col>
+			</Row>
+			
 			<br/>
 			<br/>
         </div>
